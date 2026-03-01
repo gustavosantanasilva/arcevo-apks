bf55ea3b76312257602a51cd79f3749d7583bf1b
Gustavo Santos <gustafsantossilva@gmail.com>
2026-03-01 19:06:41 -0300

    sda

--- netlify/functions/server.js
+++
@@ -1,9 +0,0 @@
-const serverless = require('serverless-http');
-const { app, init } = require('../../server');
-
-const handler = serverless(app);
-
-exports.handler = async (event, context) => {
-  await init();
-  return handler(event, context);
-};
